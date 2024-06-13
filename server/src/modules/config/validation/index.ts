857a2c5d557ef91e309daa8ef30a98f3ac171b1a
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-06-13 18:17:04 +0300

    add missing validation schema

--- server/src/modules/config/validation/index.ts
+++ server/src/modules/config/validation/index.ts
@@ -2,7 +2,11 @@ import * as Joi from 'joi';
 
 import { appValidationSchema } from './app';
 import { postgresValidationSchema } from './postgres';
+import { jwtValidationSchema } from './jwt';
+import { authValidationSchema } from './auth';
 
 export const configValidationSchema = Joi.object()
   .concat(appValidationSchema)
-  .concat(postgresValidationSchema);
+  .concat(postgresValidationSchema)
+  .concat(jwtValidationSchema)
+  .concat(authValidationSchema)
