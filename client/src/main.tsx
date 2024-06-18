ded749ea35b1e0c49a14c6afafc220291191f628
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-06-18 12:11:17 +0300

    add path alias

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import './index.css';
-import './core/config/index.ts';
+import '@/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
