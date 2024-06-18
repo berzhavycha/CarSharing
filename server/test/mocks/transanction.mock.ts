50024bbd7caca1ce74732e1308cf00848ebcd50e
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-06-18 21:03:39 +0300

    cover TransanctionService with unit tests

---
+++ server/test/mocks/transanction.mock.ts
@@ -0,0 +1,12 @@
+import { User, Rental, Transaction } from "@/entities";
+import { TransactionType } from "@/helpers";
+
+export const mockTransanction = {
+    id: 'transanction-id',
+    amount: 400,
+    description: 'Description',
+    type: TransactionType.RENTAL_PAYMENT,
+    createdAt: new Date(),
+    user: new User(),
+    rental: new Rental(),
+} as Transaction
---
+++ server/test/services/transactions.service.spec.ts
@@ -0,0 +1,51 @@
+import { Test } from '@nestjs/testing'
+import { TransactionsService } from '@/services';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { mockTransanction, repositoryMock } from '../mocks';
+import { Transaction } from '@/entities';
+
+describe('TransanctionsService', () => {
+    let transactionsService: TransactionsService;
+    let transactionsRepository: Repository<Transaction>;
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                TransactionsService,
+                {
+                    provide: getRepositoryToken(Transaction),
+                    useValue: repositoryMock,
+                },
+            ],
+        }).compile();
+
+        transactionsService = module.get<TransactionsService>(TransactionsService);
+        transactionsRepository = module.get<Repository<Transaction>>(getRepositoryToken(Transaction));
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should be defined', () => {
+        expect(transactionsService).toBeDefined();
+    });
+
+    describe('createTransaction', () => {
+        it('should create a role', async () => {
+            const mockEntityManager = {
+                save: jest.fn(),
+            };
+
+            jest.spyOn(transactionsRepository, 'create').mockReturnValue(mockTransanction);
+            jest.spyOn(mockEntityManager, 'save').mockResolvedValue(mockTransanction);
+
+            const result = await transactionsService.createTransaction({ ...mockTransanction }, mockEntityManager as unknown as EntityManager);
+
+            expect(result).toBe(mockTransanction);
+            expect(transactionsRepository.create).toHaveBeenCalledWith({ ...mockTransanction });
+            expect(mockEntityManager.save).toHaveBeenCalledWith(mockTransanction);
+        });
+    });
+})
