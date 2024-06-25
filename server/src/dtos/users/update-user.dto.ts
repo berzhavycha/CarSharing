06b0b4979ec352fcb734e49412a98c001903a0f9
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-06-25 13:16:48 +0300

    refactor update user on server

--- server/src/dtos/users/update-user.dto.ts
+++ server/src/dtos/users/update-user.dto.ts
@@ -1,22 +1,22 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
-import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import { IsOptional, IsString, MinLength } from 'class-validator';
 
 import { authErrorMessages, PASSWORD_MIN_LENGTH } from '@/helpers';
 
 import { RegisterUserDto } from '../auth';
 
 export class UpdateUserDto extends PartialType(
-  OmitType(RegisterUserDto, ['role']),
+  OmitType(RegisterUserDto, ['role', 'password']),
 ) {
   @IsOptional()
   readonly picture?: Express.Multer.File;
 
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
   @MinLength(PASSWORD_MIN_LENGTH, { message: authErrorMessages.SMALL_PASSWORD })
   oldPassword?: string;
 
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
   @MinLength(PASSWORD_MIN_LENGTH, { message: authErrorMessages.SMALL_PASSWORD })
   newPassword?: string;
