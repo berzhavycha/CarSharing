7e713c98ef20ec76cc975a293364d04244668d85
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-01 14:37:55 +0300

    improved banners

--- client/src/pages/cs-main-page/cs-main-page.tsx
+++ client/src/pages/cs-main-page/cs-main-page.tsx
@@ -1,22 +1,26 @@
 import { FC, Suspense } from 'react';
 
-import { CSCommonContainer, CSCommonError, CSCommonSpinner, CSInitialCarList, CSMainPageBanner } from '@/components';
+import { CSCommonContainer, CSCommonError, CSCommonPrimaryButton, CSCommonSpinner, CSInitialCarList, CSMainPageBanner } from '@/components';
 
-import BannerImage from '../../../public/banner 2.png'
+import BannerImage1 from '../../../public/banner 1.png'
+import BannerImage2 from '../../../public/banner 2.png'
 import DefaultCar from '../../../public/default-car.png'
 import styled from 'styled-components';
-import { Await, useLoaderData } from 'react-router-dom';
+import { Await, useLoaderData, useNavigate } from 'react-router-dom';
 import { InitialCarsLoaderData } from './loader';
 import { UNEXPECTED_ERROR_MESSAGE } from '@/helpers';
 
 export const CSMainUserPage: FC = () => {
   const data = useLoaderData() as { data: InitialCarsLoaderData }
+  const navigate = useNavigate()
+
+  const onShowMore = (): void => navigate('/available-cars')
 
   return (
     <CSCommonContainer>
       <BannerWrappers>
-        <CSMainPageBanner title='The Best Platform for Car Sharing' backgroundImage={BannerImage} description='Ease of doing a car rental safely and reliably. Of course at a low price.' carImage={DefaultCar} buttonType='light' />
-        <CSMainPageBanner title='The Best Platform for Car Sharing' backgroundImage={BannerImage} description='Ease of doing a car rental safely and reliably. Of course at a low price.' carImage={DefaultCar} buttonType='main' />
+        <CSMainPageBanner title='The Best Platform for Car Sharing' backgroundImage={BannerImage1} description='Ease of doing a car rental safely and reliably. Of course at a low price.' carImage={DefaultCar} buttonType='main' />
+        <CSMainPageBanner title='Easy way to rent a car at a low price' backgroundImage={BannerImage2} description='Providing cheap car rental services and safe and comfortable facilities.' carImage={DefaultCar} buttonType='light' />
       </BannerWrappers>
       <CarsWrapper>
         <Suspense fallback={<CSCommonSpinner />}>
@@ -32,6 +36,9 @@ export const CSMainUserPage: FC = () => {
           </Await>
         </Suspense>
       </CarsWrapper>
+      <ShowMoreWrapper>
+        <CSCommonPrimaryButton onClick={onShowMore} content='Show more cars' />
+      </ShowMoreWrapper>
     </CSCommonContainer>
   );
 };
@@ -47,4 +54,9 @@ const CarsWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
+`
+
+const ShowMoreWrapper = styled.div`
+  margin: 40px 0;
+  text-align: center;
 `
