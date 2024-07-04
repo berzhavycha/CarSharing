3a373633ad75f857aeea9b6508822e12b389bafc
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-04 23:08:58 +0300

    improve cars service tests

---
+++ server/test/utils/local-file.ts
@@ -0,0 +1,13 @@
+import { Car, LocalFile, OriginalCar } from "@/entities";
+
+export const makeLocalFile = (details?: Partial<LocalFile>): LocalFile => {
+    return {
+        id: 'av-id',
+        filename: 'test.jpg',
+        path: 'some/path',
+        mimetype: 'image/jpeg',
+        car: new Car(),
+        originalCar: new OriginalCar(),
+        ...details
+    }
+}
