35d253d4ca73900da07cffbcd1a2a5d3b85e069b
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-05 12:45:16 +0300

    add global styles file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,12 @@
-import '@fontsource/plus-jakarta-sans';
-import '@fontsource/plus-jakarta-sans/300.css';
-import '@fontsource/plus-jakarta-sans/400.css';
-import '@fontsource/plus-jakarta-sans/500.css';
-import '@fontsource/plus-jakarta-sans/600.css';
-import '@fontsource/plus-jakarta-sans/700.css';
-import '@fontsource/plus-jakarta-sans/800.css';
+import { GlobalStyles } from '@/styles';
 import { RouterProvider } from 'react-router-dom';
-
 import { CSStoreProvider } from './context';
 import { router } from './router';
 
 function App(): JSX.Element {
   return (
     <CSStoreProvider>
+      <GlobalStyles />
       <RouterProvider router={router} />
     </CSStoreProvider>
   );
