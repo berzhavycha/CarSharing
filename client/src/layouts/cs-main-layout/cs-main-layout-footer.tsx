cac0f1b663a5a1e4e742481e5bd3a915ee36030b
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-06 19:08:59 +0300

    add responsive footer design

--- client/src/layouts/cs-main-layout/cs-main-layout-footer.tsx
+++ client/src/layouts/cs-main-layout/cs-main-layout-footer.tsx
@@ -2,9 +2,11 @@ import { FC } from 'react';
 import styled from 'styled-components';
 
 import { CSCommonContainer } from '@/components';
+import { device } from '@/styles';
 
 export const CSMainLayoutFooter: FC = () => {
   const currentYear = new Date().getFullYear();
+
   return (
     <Section>
       <CSCommonContainer>
@@ -30,10 +32,19 @@ const FooterWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media ${device.sm} {
+    flex-direction: column;
+    justify-content: center; 
+  }
 `;
 
 const Copyright = styled.span`
   color: #666;
+
+  @media ${device.md} {
+    font-size: 12px;
+  }
 `;
 
 const Links = styled.div`
@@ -43,6 +54,17 @@ const Links = styled.div`
     margin-left: 20px;
     &:hover {
       text-decoration: underline;
+    }
+
+    @media ${device.md} {
+      font-size: 12px;
+      }
+      
+    @media ${device.sm}{
+      margin-left: 0px;
+      &:last-child {
+        margin-left: 10px;
+      }
     }
   }
 `;
