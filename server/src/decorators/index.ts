e0df174106e87540099248cf04ed0036a6852c14
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-08 17:21:58 +0300

    remove unneccessary decorator

--- server/src/decorators/index.ts
+++ server/src/decorators/index.ts
@@ -1,3 +1,2 @@
 export * from './public.decorator';
 export * from './current-user.decorator';
-export * from './roles.decorator';
--- server/src/decorators/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { CustomDecorator, SetMetadata } from '@nestjs/common';
-
-import { Roles as AvailableRoles } from '@/helpers';
-
-export const ROLES_KEY = 'role';
-export const Roles = (role: AvailableRoles): CustomDecorator<string> =>
-  SetMetadata(ROLES_KEY, role);
