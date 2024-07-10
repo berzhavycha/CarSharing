e761489013d3b22fbe4b8102093f86b529fbadd0
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-10 23:23:41 +0300

    fix public file migration error

--- server/src/core/database/migrations/1720614861016-AddPublicFileEntity.ts
+++ server/src/core/database/migrations/1720614861016-AddPublicFileEntity.ts
@@ -9,6 +9,7 @@ export class AddPublicFileEntity1720614861016 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "public_file" ADD CONSTRAINT "FK_51276d2f5c1f0754463468814d3" FOREIGN KEY ("car_id") REFERENCES "rental"."cars"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "public_file" ADD CONSTRAINT "FK_358ab9f3bd5605a89b3b9339c34" FOREIGN KEY ("original_car_id") REFERENCES "rental"."original_cars"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "public_file" ADD CONSTRAINT "FK_d78f5b649b1e8c090e08fbf597a" FOREIGN KEY ("avatar_id") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`UPDATE "auth"."users" SET "avatar_id" = NULL WHERE "avatar_id" IS NOT NULL AND "avatar_id" NOT IN (SELECT "id" FROM "public_file")`);
         await queryRunner.query(`ALTER TABLE "auth"."users" ADD CONSTRAINT "FK_c3401836efedec3bec459c8f818" FOREIGN KEY ("avatar_id") REFERENCES "public_file"("id") ON DELETE SET NULL ON UPDATE CASCADE`);
     }
 
@@ -20,5 +21,4 @@ export class AddPublicFileEntity1720614861016 implements MigrationInterface {
         await queryRunner.query(`DROP TABLE "public_file"`);
         await queryRunner.query(`ALTER TABLE "auth"."users" ADD CONSTRAINT "FK_c3401836efedec3bec459c8f818" FOREIGN KEY ("avatar_id") REFERENCES "local_files"("id") ON DELETE SET NULL ON UPDATE CASCADE`);
     }
-
 }
