618a582b5ad42fa03551deccf3bf22f2536e788c
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-11 21:37:27 +0300

    add credit card form

--- client/src/components/cs-common/cs-common-payment-form/cs-common-payment-form.tsx
+++ client/src/components/cs-common/cs-common-payment-form/cs-common-payment-form.tsx
@@ -1,9 +1,11 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import styled from 'styled-components';
 
 import { CSCommonForm } from '@/components/cs-common';
 import { device } from '@/styles';
 
+import Cards, { Focused } from 'react-credit-cards-2';
+import 'react-credit-cards-2/dist/es/styles-compiled.css';
 import { BaseSection, SectionDescription, SectionTitle } from '../cs-common-section';
 
 type Props = {
@@ -13,15 +15,67 @@ type Props = {
 };
 
 export const CSCommonPaymentForm: FC<Props> = ({ title, description, submitButtonContent }) => {
+  const [cardDetails, setCardDetails] = useState({
+    number: '',
+    name: '',
+    expiry: '',
+    cvc: '',
+  });
+
+  const [focused, setFocused] = useState<string>('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setCardDetails({
+      ...cardDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleInputFocus = (e: React.FocusEvent<HTMLInputElement>): void => {
+    setFocused(e.target.name);
+  };
+
   return (
     <FormInfoWrapper>
       <SectionTitle>{title}</SectionTitle>
       <SectionDescription>{description}</SectionDescription>
+      <Cards
+        number={cardDetails.number}
+        name={cardDetails.name}
+        expiry={cardDetails.expiry}
+        cvc={cardDetails.cvc}
+        focused={focused as Focused}
+      />
       <PaymentFormBlocks>
-        <CSCommonForm.Input name="cardNumber" label="Card Number" />
-        <CSCommonForm.Input name="expirationDate" label="Expiration Date" />
-        <CSCommonForm.Input name="cardHolder" label="Card Holder" />
-        <CSCommonForm.Input name="CVC" label="CVC" type="number" />
+        <CSCommonForm.Input
+          name="number"
+          label="Card Number"
+          value={cardDetails.number}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <CSCommonForm.Input
+          name="expiry"
+          label="Expiration Date"
+          value={cardDetails.expiry}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <CSCommonForm.Input
+          name="name"
+          label="Card Holder"
+          value={cardDetails.name}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
+        <CSCommonForm.Input
+          name="cvc"
+          label="CVC"
+          type="number"
+          value={cardDetails.cvc}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+        />
       </PaymentFormBlocks>
       <CSCommonForm.SubmitButton content={submitButtonContent} />
     </FormInfoWrapper>
