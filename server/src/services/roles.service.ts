678e5d05b984385969019310edfc12f0a3b5b627
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-14 11:54:12 +0300

    cover roles service with logs

--- server/src/services/roles.service.ts
+++ server/src/services/roles.service.ts
@@ -3,19 +3,26 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Role } from '@/entities';
+import { LoggerService } from './logger.service';
 
 @Injectable()
 export class RolesService {
   constructor(
     @InjectRepository(Role) private readonly rolesRepository: Repository<Role>,
-  ) {}
+    private readonly loggerService: LoggerService
+  ) { }
 
   async createRole(name: string): Promise<Role> {
-    const role = this.rolesRepository.create({
-      name,
-    });
+    try {
+      const role = this.rolesRepository.create({
+        name,
+      });
 
-    return this.rolesRepository.save(role);
+      return this.rolesRepository.save(role);
+    } catch (error) {
+      this.loggerService.error(`Error creating a role: ${error.message}`, error.stack);
+      throw error;
+    }
   }
 
   async findByName(name: string): Promise<Role | null> {
