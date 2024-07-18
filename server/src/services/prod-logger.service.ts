873681cdb4c200acd0bda08045559ea518b2b965
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-18 20:16:25 +0300

    add console logging for prod

--- server/src/services/prod-logger.service.ts
+++ server/src/services/prod-logger.service.ts
@@ -1,65 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import * as winston from 'winston';
-import * as AWS from 'aws-sdk';
-import WinstonCloudWatch from 'winston-cloudwatch';
 
 @Injectable()
 export class ProdLoggerService {
-    private readonly logger: winston.Logger;
+    constructor() { }
 
-    constructor(private configService: ConfigService) {
-        const awsRegion = this.configService.get<string>('AWS_REGION');
-        const logGroupName = this.configService.get<string>('LOG_GROUP_NAME');
-        const logStreamName = this.configService.get<string>('LOG_STREAM_NAME');
-
-        AWS.config.update({
-            accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
-            region: awsRegion
-        });
-        console.log({
-            logGroupName,
-            logStreamName,
-            awsRegion,
-            jsonMessage: true,
-        })
-
-        this.logger = winston.createLogger({
-            level: 'info',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json(),
-            ),
-            transports: [
-                new WinstonCloudWatch({
-                    logGroupName,
-                    logStreamName,
-                    awsRegion,
-                    jsonMessage: true,
-                }),
-            ],
-        });
+    private formatMessage(level: string, message: string, trace?: string): string {
+        const timestamp = new Date().toISOString();
+        const formattedMessage = `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+        if (trace) {
+            return `${formattedMessage}\nTrace: ${trace}`;
+        }
+        return formattedMessage;
     }
 
     log(message: string): void {
-        try {
-            console.log("TESTING")
-            this.logger.info(message);
-        } catch (error) {
-            console.error('Logging failed:', error);
-        }
+        console.log(this.formatMessage('info', message));
     }
 
     error(message: string, trace?: string): void {
-        this.logger.error(message, { trace });
+        console.error(this.formatMessage('error', message, trace));
     }
 
     warn(message: string): void {
-        this.logger.warn(message);
+        console.warn(this.formatMessage('warn', message));
     }
 
     debug(message: string): void {
-        this.logger.debug(message);
+        console.debug(this.formatMessage('debug', message));
     }
 }
