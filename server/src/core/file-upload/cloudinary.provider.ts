a4561f80027441d3303c01ed5ed029e56eb4f13a
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-18 10:44:26 +0300

    update cloudinary provider

--- server/src/core/file-upload/cloudinary.provider.ts
+++ server/src/core/file-upload/cloudinary.provider.ts
@@ -1,12 +1,15 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { v2 as cloudinary, ConfigOptions } from 'cloudinary';
 
 export const CloudinaryProvider = {
+  imports: [ConfigModule],
   provide: 'CLOUDINARY',
-  useFactory: (): ConfigOptions => {
+  useFactory: (configService: ConfigService): ConfigOptions => {
     return cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
+      cloud_name: configService.get<string>('CLOUDINARY_NAME'),
+      api_key: configService.get<string>('CLOUDINARY_API_KEY'),
+      api_secret: configService.get<string>('CLOUDINARY_API_SECRET'),
     });
   },
+  inject: [ConfigService],
 };
