2ccb567795feeb01b361b036ef46c52a1665bc13
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-18 21:17:03 +0300

    update logger instatiating in dev logger service

--- server/src/services/dev-logger.service.ts
+++ server/src/services/dev-logger.service.ts
@@ -3,8 +3,6 @@ import * as path from 'path';
 import * as winston from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
-import { NODE_ENV } from '@/helpers';
-
 const logDirectory = path.join(process.cwd(), 'src/core/logs');
 
 @Injectable()
@@ -12,48 +10,37 @@ export class DevLoggerService {
   private readonly logger: winston.Logger | null;
 
   constructor() {
-    this.logger =
-      process.env.NODE_ENV === NODE_ENV.production
-        ? null
-        : winston.createLogger({
-            level: 'debug',
-            format: winston.format.combine(
-              winston.format.timestamp(),
-              winston.format.json(),
-            ),
-            transports: [
-              new DailyRotateFile({
-                filename: path.join(logDirectory, 'application-%DATE%.log'),
-                datePattern: 'YYYY-MM-DD',
-                zippedArchive: true,
-                maxSize: '20m',
-                maxFiles: '14d',
-              }),
-            ],
-          });
+    this.logger = winston.createLogger({
+      level: 'debug',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+      transports: [
+        new DailyRotateFile({
+          filename: path.join(logDirectory, 'application-%DATE%.log'),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '14d',
+        }),
+      ],
+    });
   }
 
   log(message: string): void {
-    if (this.logger) {
-      this.logger.info(message);
-    }
+    this.logger.info(message);
   }
 
   error(message: string, trace?: string): void {
-    if (this.logger) {
-      this.logger.error(message, { trace });
-    }
+    this.logger.error(message, { trace });
   }
 
   warn(message: string): void {
-    if (this.logger) {
-      this.logger.warn(message);
-    }
+    this.logger.warn(message);
   }
 
   debug(message: string): void {
-    if (this.logger) {
-      this.logger.debug(message);
-    }
+    this.logger.debug(message);
   }
 }
