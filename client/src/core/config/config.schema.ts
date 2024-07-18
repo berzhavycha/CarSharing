289aef3dfff7aff1cf4fd7819dc979849c6139f9
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-18 09:00:43 +0300

    update configSchema

--- client/src/core/config/config.schema.ts
+++ client/src/core/config/config.schema.ts
@@ -6,6 +6,9 @@ const configSchema = z.object({
   API_BASE_URL: z.string().refine((value) => value.trim() !== '', {
     message: 'API_BASE_URL cannot be an empty string',
   }),
+  CLOUDINARY_NAME: z.string().refine((value) => value.trim() !== '', {
+    message: 'CLOUDINARY_NAME cannot be an empty string',
+  }),
   PASSWORD_MIN_LENGTH: z
     .string()
     .transform((val) => parseInt(val, 10))
@@ -16,9 +19,6 @@ const configSchema = z.object({
   ADMIN_TRANSACTIONS_PAGINATION_LIMIT: paginationSchema.default('10'),
   USER_CARS_PAGINATION_LIMIT: paginationSchema.default('9'),
   USER_RENTAL_HISTORY_PAGINATION_LIMIT: paginationSchema.default('10'),
-  CLOUDINARY_NAME: z.string().refine((value) => value.trim() !== '', {
-    message: 'CLOUDINARY_NAME cannot be an empty string',
-  }),
 });
 
 export type Config = z.infer<typeof configSchema>;
