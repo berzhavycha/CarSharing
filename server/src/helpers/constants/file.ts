d6d35e3e7b7d66bcde561ff72e896e359fa2d3b8
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-19 08:21:52 +0300

    increase cloudinary timeout

--- server/src/helpers/constants/file.ts
+++ server/src/helpers/constants/file.ts
@@ -3,3 +3,5 @@ import { ONE_MB } from './general';
 export const defaultFileLimits = {
   fileSize: 4 * ONE_MB,
 };
+
+export const CLOUDINARY_TIMEOUT = 60000
--- server/src/services/cloudinary.service.ts
+++ server/src/services/cloudinary.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
 
 import { CloudinaryResponse } from '@/types';
+import { CLOUDINARY_TIMEOUT } from '@/helpers';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const streamifier = require('streamifier');
@@ -10,16 +11,21 @@ const streamifier = require('streamifier');
 export class CloudinaryService {
   uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
     return new Promise<CloudinaryResponse>((resolve, reject) => {
-      cloudinary.uploader.upload_stream(
-        { resource_type: 'auto' },
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          resource_type: 'auto',
+          timeout: CLOUDINARY_TIMEOUT
+        },
         (error, result) => {
           if (error) return reject(error);
           resolve(result);
-        }
-      ).end(file.buffer);
+        },
+      );
+
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
     });
   }
-  
+
   deleteFile(publicId: string): Promise<CloudinaryResponse> {
     return new Promise<CloudinaryResponse>((resolve, reject) => {
       cloudinary.uploader.destroy(publicId, (error, result) => {
