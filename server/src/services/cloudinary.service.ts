441911813711dda073bf67525bb02bdf494b7fc1
Andriy Berzhavych <140008539+berzhavycha@users.noreply.github.com>
2024-07-19 11:36:24 +0300

    move back to basic cloudinary setup

--- server/src/services/cloudinary.service.ts
+++ server/src/services/cloudinary.service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
+
 import { CloudinaryResponse } from '@/types';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const streamifier = require('streamifier');
 
 @Injectable()
 export class CloudinaryService {
-  async uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
-    console.log("CLOUDINARY FILE DATA", file);
+  uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
+    console.log("CLOUDINARY_FILE_DATA", file.filename)
 
-    const base64String = file.buffer.toString('base64');
-    const dataUri = `data:${file.mimetype};base64,${base64String}`;
+    return new Promise<CloudinaryResponse>((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
 
-    try {
-      const result = await cloudinary.uploader.upload(dataUri, { timeout: 60000 });
-      return result;
-    } catch (error) {
-      console.error('Failed to upload file to Cloudinary:', error);
-      throw new Error(`Failed to upload file to Cloudinary: ${error.message || 'Unknown error'}`);
-    }
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    });
   }
 
   deleteFile(publicId: string): Promise<CloudinaryResponse> {
